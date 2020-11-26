a7314621fe9c2e6dc751c44661e5509126392204
jacelyn.angraini <jacelyn.angraini@binus.ac.id>
2020-11-26 12:14:06 +0700

    First commit

---
+++ app/src/main/java/com/jacelyn/notificationservice/MainActivity.java
@@ -0,0 +1,26 @@
+package com.jacelyn.notificationservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
+                getResources().getString(R.string.quote));
+        startService(intent);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(R.string.success);
+    }
+}
